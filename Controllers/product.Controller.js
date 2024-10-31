0c004493f38ce3a98fe5094672f2063a43f25b66
very97 <very97@naver.com>
2024-10-31 16:56:28 +0900

    pagination complete

--- Controllers/product.Controller.js
+++ Controllers/product.Controller.js
@@ -1,5 +1,7 @@
 const Product = require("../Models/Product");
 const productController = async () => {};
+const PAGE_SIZE = 1;
+let response = { status: "success" };
 
 productController.createProduct = async (req, res) => {
   try {
@@ -37,8 +39,29 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
   try {
+    const { page, name } = req.query;
+
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(cond);
+
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      // 최종 몇개 페이지
+      // 데이터가 총 몇개 있는지
+      const totalItemNum = await Product.countDocuments(cond);
+
+      //데이터 총 개수
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+
+    const productList = await query.exec();
+    response.data = productList;
+
     const products = await Product.find({});
-    res.status(200).json({ status: "success", data: products });
+    //console.log(productList);
+    //res.status(200).json({ status: "success", data: productList });
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
